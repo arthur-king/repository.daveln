c64ef698b01c9b4f635970d22e777a3a7b8a3dc7
David Le <tonyjasonnguyen@gmail.com>
2014-10-03 16:41:04 -0700

    update phim3s

--- plugin.video.netmovie/default.py
+++ plugin.video.netmovie/default.py
@@ -28,21 +28,21 @@ fanart     = xbmc.translatePath(os.path.join(home, 'fanart.jpg'))
 icon       = xbmc.translatePath(os.path.join(home, 'icon.png'))
 logos      = xbmc.translatePath(os.path.join(home, 'logos\\'))
 phim3s     = 'http://phim3s.net/'
-pagestr    = map(str, range(1,220))
 
 def categories():
-		addDir('[COLOR lime]Phim3s.net[/COLOR]',phim3s,2,logos + 'phim3s.png')
+		addDir('[COLOR lime]phim3s.net[/COLOR]',phim3s,2,logos + 'phim3s.png')
 		
 def search():
-    try:
-        keyb = xbmc.Keyboard('', '[COLOR yellow]Enter search text[/COLOR]')
-        keyb.doModal()
-        if (keyb.isConfirmed()):
-			searchText = urllib.quote_plus(keyb.getText())
-        url = phim3s + 'search?keyword='+searchText
-        index(url)
-    except: pass
-
+		if 'phim3s' in url:
+				try:
+						keyb = xbmc.Keyboard('', '[COLOR yellow]Enter search text[/COLOR]')
+						keyb.doModal()
+						if (keyb.isConfirmed()):
+								searchText = urllib.quote_plus(keyb.getText())
+						url = phim3s + 'search?keyword=' + searchText
+						index(url)
+				except: pass
+				
 def main():
 		if 'phim3s' in url:
 				addDir('[COLOR yellow]Tìm Phim[/COLOR][B]   [COLOR lime]>[/COLOR][COLOR orange]>[/COLOR][COLOR blue]>[/COLOR][COLOR magenta]>[/COLOR]   [/B][COLOR yellow]Movie Search[/COLOR]',phim3s,1,logos + 'phim3s.png')
@@ -53,7 +53,7 @@ def main():
 				addDir('[COLOR cyan]Phim Chiếu Rạp[/COLOR]',phim3s + 'danh-sach/phim-chieu-rap',3,logos + 'phim3s.png')
 				addDir('[COLOR pink]Phim Mới[/COLOR]',phim3s + 'danh-sach/phim-moi',3,logos + 'phim3s.png')
 				addDir('[COLOR coral]Phim Thuyết Minh[/COLOR]',phim3s + 'danh-sach/phim-thuyet-minh',4,logos + 'phim3s.png')
-
+				
 def sub():
 		if 'phim3s' in url:
 				if 'the-loai' in url:
@@ -103,243 +103,33 @@ def sub():
 						addDir('[COLOR silver]Phim Mới 2014[/COLOR]',phim3s + 'danh-sach/phim-2014',4,logos + 'phim3s.png')
 						addDir('[COLOR violet]Phim Mới 2013[/COLOR]',phim3s + 'danh-sach/phim-2013',4,logos + 'phim3s.png')
 						addDir('[COLOR cyan]Phim Mới 2012[/COLOR]',phim3s + 'danh-sach/phim-2012',4,logos + 'phim3s.png')
-		
-def pagelist():
-		if 'phim3s' in url:
-				if 'phim-am-nhac' in url:
-						i=0
-						for i in range(3):
-								addDir('[COLOR yellow]Phim Âm Nhạc[/COLOR] - [COLOR lime]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-am-nhac/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-chien-tranh' in url:
-						i=0
-						for i in range(9):
-								addDir('[COLOR lime]Phim Chiến Tranh[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-chien-tranh/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-co-trang' in url:
-						i=0
-						for i in range(14):
-								addDir('[COLOR orange]Phim Cổ Trang[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-co-trang/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-hai-huoc' in url:
-						i=0
-						for i in range(47):
-								addDir('[COLOR blue]Phim Hài Hước[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-hai-huoc/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-hanh-dong' in url:
-						i=0
-						for i in range(57):
-								addDir('[COLOR magenta]Phim Hành Động[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-hanh-dong/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-hinh-su' in url:
-						i=0
-						for i in range(130):
-								addDir('[COLOR tan]Phim Hình Sự[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-hinh-su/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-hoat-hinh' in url:
-						i=0
-						for i in range(40):
-								addDir('[COLOR chocolate]Phim Hoạt Hình[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-hoat-hinh/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-kinh-di' in url:
-						i=0
-						for i in range(21):
-								addDir('[COLOR cyan]Phim Kinh Dị[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-kinh-di/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-phieu-luu' in url:
-						i=0
-						for i in range(22):
-								addDir('[COLOR violet]Phim Phiêu Lưu[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-phieu-luu/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-vien-tuong' in url:
-						i=0
-						for i in range(19):
-								addDir('[COLOR gold]Phim Viễn Tưởng[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-vien-tuong/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-vo-thuat' in url:
-						i=0
-						for i in range(20):
-								addDir('[COLOR white]Phim Võ Thuật[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-vo-thuat/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-tam-ly' in url:
-						i=0
-						for i in range(68):
-								addDir('[COLOR silver]Phim Tâm Lý[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-tam-ly/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-than-thoai' in url:
-						i=0
-						for i in range(4):
-								addDir('[COLOR olive]Phim Thần Thoại[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-than-thoai/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-tv-show' in url:
-						i=0
-						for i in range(18):
-								addDir('[COLOR pink]Phim TV Show[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'the-loai/phim-tv-show/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-anh' in url:
-						i=0
-						for i in range(2):
-								addDir('[COLOR white]Phim Anh[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-anh/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-an-do' in url:
-						i=0
-						for i in range(3):
-								addDir('[COLOR gold]Phim Ấn Độ[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-an-do/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-han-quoc' in url:
-						i=0
-						for i in range(19):
-								addDir('[COLOR violet]Phim Hàn Quốc[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-han-quoc/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-hong-kong' in url:
-						i=0
-						for i in range(13):
-								addDir('[COLOR cyan]Phim Hồng Kông[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-hong-kong/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-khac' in url:
-						i=0
-						for i in range(5):
-								addDir('[COLOR chocolate]Phim Khác[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-khac/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-my' in url:
-						i=0
-						for i in range(90):
-								addDir('[COLOR tan]Phim Mỹ[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-my/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-nhat-ban' in url:
-						i=0
-						for i in range(34):
-								addDir('[COLOR magenta]Phim Nhật Bản[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-nhat-ban/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-phap' in url:
-						i=0
-						for i in range(2):
-								addDir('[COLOR blue]Phim Pháp[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-phap/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-thai-lan' in url:
-						i=0
-						for i in range(3):
-								addDir('[COLOR orange]Phim Thái Lan[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-thai-lan/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-trung-quoc' in url:
-						i=0
-						for i in range(27):
-								addDir('[COLOR lime]Phim Trung Quốc[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-trung-quoc/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-viet-nam' in url:
-						i=0
-						for i in range(17):
-								addDir('[COLOR yellow]Phim Việt Nam[/COLOR] - [COLOR lime]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'quoc-gia/phim-viet-nam/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-le':
-						i=0
-						for i in range(131):
-								addDir('[COLOR lime]Toàn Bộ Phim Lẻ[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-le/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-le/?year=2014':
-						i=0
-						for i in range(5):
-								addDir('[COLOR silver]Phim Lẻ 2014[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-le/page-' + pagestr[i] + '/?year=2014',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-le/?year=2013':
-						i=0
-						for i in range(17):
-								addDir('[COLOR violet]Phim Lẻ 2013[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-le/page-' + pagestr[i] + '/?year=2013',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-le/?year=2012':
-						i=0
-						for i in range(21):
-								addDir('[COLOR cyan]Phim Lẻ 2012[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-le/page-' + pagestr[i] + '/?year=2012',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-bo':
-						i=0
-						for i in range(79):
-								addDir('[COLOR lime]Toàn Bộ Phim Bộ[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-bo/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-bo/?year=2014':
-						i=0
-						for i in range(4):
-								addDir('[COLOR silver]Phim Bộ 2014[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-bo/page-' + pagestr[i] + '/?year=2014',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-bo/?year=2013':
-						i=0
-						for i in range(12):
-								addDir('[COLOR violet]Phim Bộ 2013[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-bo/page-' + pagestr[i] + '/?year=2013',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-bo/?year=2012':
-						i=0
-						for i in range(22):
-								addDir('[COLOR cyan]Phim Bộ 2012[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-bo/page-' + pagestr[i] + '/?year=2012',5,logos + 'phim3s.png')
-								i+=1						
-				if url==phim3s + 'danh-sach/phim-chieu-rap':
-						i=0
-						for i in range(22):
-								addDir('[COLOR lime]Toàn Bộ Phim Chiếu Rạp[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-chieu-rap/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-chieu-rap/?year=2014':
-						i=0
-						for i in range(2):
-								addDir('[COLOR silver]Phim Chiếu Rạp 2014[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-chieu-rap/page-' + pagestr[i] + '/?year=2014',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-chieu-rap/?year=2013':
-						i=0
-						for i in range(7):
-								addDir('[COLOR violet]Phim Chiếu Rạp 2013[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-chieu-rap/page-' + pagestr[i] + '/?year=2013',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-chieu-rap/?year=2012':
-						i=0
-						for i in range(8):
-								addDir('[COLOR cyan]Phim Chiếu Rạp 2012[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-chieu-rap/page-' + pagestr[i] + '/?year=2012',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-moi':
-						i=0
-						for i in range(209):
-								addDir('[COLOR lime]Toàn Bộ Phim Mới[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-moi/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-2014':
-						i=0
-						for i in range(9):
-								addDir('[COLOR silver]Phim Mới 2014[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-moi/page-' + pagestr[i] + '/?year=2014',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-2013':
-						i=0
-						for i in range(28):
-								addDir('[COLOR violet]Phim Mới 2013[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-moi/page-' + pagestr[i] + '/?year=2013',5,logos + 'phim3s.png')
-								i+=1
-				if url==phim3s + 'danh-sach/phim-2012':
-						i=0
-						for i in range(43):
-								addDir('[COLOR cyan]Phim Mới 2012[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-moi/page-' + pagestr[i] + '/?year=2012',5,logos + 'phim3s.png')
-								i+=1
-				if 'phim-thuyet-minh' in url:
-						i=0
-						for i in range(48):
-								addDir('[COLOR coral]Phim Thuyết Minh[/COLOR] - [COLOR yellow]Trang ' + pagestr[i] + '[/COLOR]',phim3s + 'danh-sach/phim-thuyet-minh/page-' + pagestr[i],5,logos + 'phim3s.png')
-								i+=1
-						
+				
 def index(url):
 		req = urllib2.Request(url)
 		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
 		response = urllib2.urlopen(req, timeout=90)
 		link=response.read()
 		response.close()
-		if 'phim3s' in url:		
+		if 'phim3s' in url:
 				match=re.compile("<div class=\"inner\"><a href=\"(.*?)\" title=\"([^\"]*)\"><img src=\"(.+?)\"").findall(link)
 				for url,name,thumbnail in match:
 						addDir('[COLOR yellow]' + name + '[/COLOR]',('%s%sxem-phim' % (phim3s, url)),6,thumbnail)					
-		
+				match=re.compile("<span class=\"item\"><a href=\"([^\"]*)\">(\d+)<\/a><\/span>").findall(link)
+				for url,name in match:
+						addDir('[COLOR lime]Trang ' + name + '[/COLOR]',('%s%s' % (phim3s, url)),4,logos + 'phim3s.png')					
+						
 def vlinks(url,name):
 		req = urllib2.Request(url)
 		req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
 		response = urllib2.urlopen(req, timeout=90)
 		link=response.read()
 		response.close()
-		if 'phim3s' in url:		
-				thumbnail=re.compile("<meta property=\"og:image\" content=\"([^\"]*)\"").findall(link)		
+		if 'phim3s' in url:
+				thumbnail=re.compile("<meta property=\"og:image\" content=\"([^\"]*)\"").findall(link)[0]		
 				match=re.compile("a data-type=\"watch\" data-episode-id.+?href=\"([^\"]*)\" title=\"(.*?)\"").findall(link)
 				for url,title in match:
-						addLink(('%s   -   %s' % ('[COLOR lime]' + title + '[/COLOR]',name )),('%s%svideo.mp4' % (phim3s, url)),thumbnail[-1])
-				
+						addLink(('%s   -   %s' % ('[COLOR lime]' + title + '[/COLOR]',name )),('%s%svideo.mp4' % (phim3s, url)),thumbnail)		
+						
 def get_params():
         param=[]
         paramstring=sys.argv[2]
@@ -409,17 +199,13 @@ elif mode==2:
 elif mode==3:
         print ""
         sub()
-
-elif mode==4:
-        print ""
-        pagelist()
 				
-elif mode==5:
+elif mode==4:
         print ""+url
         index(url)
         
 elif mode==6:
         print ""+url
         vlinks(url,name)
-
+		
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
